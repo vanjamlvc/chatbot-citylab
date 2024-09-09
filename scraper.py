d9a8aa270304e8ff868f85b041e1a7ea9fbb4a0d
Niklas-commits <nikla@LAPTOP-NMCV9VK2.fritz.box>
2024-09-09 17:42:07 +0200

    modified:
    file: scraper.py
    class: WebScraper
    
    added a function [clean_up_result] to clean up the scraped content
    the function drops duplicates and joins text which was scraped from the same page but different texts aslong as they had the same URL (page on which the html tag was found) and the same Link (adress the text in the html tag refers to)

--- scraper.py
+++ scraper.py
@@ -20,8 +20,18 @@ class WebScraper:
         self.extracted_texts = set()  # Set zum Vermeiden von doppelten Texten
         self.results = []  # Liste für die Ergebnisse (für DataFrame)
 
+
     # Funktion zum Abrufen und Parsen der Seite
     def fetch_page(self, url:str) -> Optional[str]:
+        """
+        tries to open the given url and returns the content as string
+        
+        Parameters
+        ----------
+        url : str
+            Uniform Resource Locator with which an attempt is made to call up content
+        """
+
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -29,16 +39,34 @@ class WebScraper:
         except requests.RequestException as e:
             print(f"Fehler beim Abrufen der Seite: {e}")
             return None
+        
 
     # Funktion zum Finden der Keywords im Text
     def search_keywords(self, text:str) -> bool:
+        """
+        checks whether one of the keywords given to the web scraper occurs in the text 
+
+        Parameters
+        ----------
+        text : str
+            str in which the keywords are searched for
+        """
+
         for keyword in self.keywords:
             if re.search(r'\b' + re.escape(keyword) + r'\b', text, re.IGNORECASE):
                 return True
         return False
+    
 
     # Funktion, um alle relevanten Links und Texte von einem Parent zu extrahieren
     def extract_from_parent(self, parent:bs4.element.Tag, current_url:str) -> list:
+        """
+        Parameters
+        ----------
+        parent : bs4.element.Tag
+            
+        current_url : str
+        """
         result = []
         # Durchlaufe alle untergeordneten Tags des Parent
         for descendant in parent.descendants:
@@ -53,9 +81,17 @@ class WebScraper:
                     self.extracted_texts.add(text)
                     result.append((current_url, text, ""))  # Leerer Link, da es sich um normalen Text handelt
         return result
+    
 
     # Funktion, um alle relevanten Links und Texte von einer Seite zu extrahieren
     def extract_links_and_texts(self, soup:BeautifulSoup, base_url:str) -> list:
+        """
+        Parameters
+        ----------
+        soup : bs4.BeautifulSoup
+            
+        base_url : str
+        """
         results = []
 
         # 1. Durchsuche alle <p>-Tags nach Stichwörtern
@@ -80,9 +116,17 @@ class WebScraper:
                     results.append((base_url, text, absolute_href))
 
         return results
+    
 
     # Funktion zum Scrapen von URLs bis zur maximalen Tiefe
     def scrape(self, url:str, depth:int=0) -> None:
+        """
+        Parameters
+        ----------
+        url : str
+            
+        depth : int
+        """
         if depth > self.max_depth or url in self.visited_urls:
             return  # Breche ab, wenn die maximale Tiefe erreicht ist oder URL schon besucht wurde
 
@@ -107,10 +151,21 @@ class WebScraper:
             if link:
                 self.scrape(link, depth + 1)
 
+
     # Funktion zum Speichern der Ergebnisse als CSV oder JSON
     def save_results_to_file(self, filename:str, folder:str=None, filetype:str= "json") -> None:
+        """
+        Parameters
+        ----------
+        filename : str
+            
+        folder : str
+
+        filetype : str
+        """
         # Erstelle DataFrame aus den Ergebnissen
         df = pd.DataFrame(self.results, columns=['URL', 'Text', 'Link'])
+        df = self.clean_up_result(df, dup_column= "Text", prefer_column="Link")
         if folder:
             if not os.path.exists(f"./{folder}"):
                 os.mkdir(f"./{folder}")
@@ -136,6 +191,46 @@ class WebScraper:
                 print(f"Ergebnisse wurden in {filename} gespeichert.")
             except IOError:
                 print(f"Error: {filename} konnte nicht erstellt werden")
+
+
+    # Funktion zum kürzen eines DataFrames mit Scrape Results durch löschen von Text Dopplungen 
+    def clean_up_result(self, df:pd.DataFrame, dup_column:str, prefer_column:str=None) -> pd.DataFrame:
+        """
+        Parameters
+        ----------
+        df : pd.DataFrame
+
+        dup_column : str
+            Name of the column from which duplicates are to be deleted
+
+        prefer_column : str
+            Name of the column whose content should be retained when deleting duplicates from the dupc_column
+
+        """
+        def select_row(group):
+            # Behalte die Zeilen mit einem gültigen Wert in Spalte prefer_column, falls vorhanden
+            non_na_rows = group[group[prefer_column].notna()]
+            
+            # Wenn es mindestens eine gültige Zeile gibt, behalte die erste
+            if len(non_na_rows) > 0:
+                return non_na_rows.iloc[0]
+            # Wenn alle Zeilen in Spalte prefer_column NaN sind, behalte einfach die erste Zeile der Gruppe
+            else:
+                return group.iloc[0]
+            
+        def merge_text_on_adress(df):
+            df_merged = df.groupby(["URL", "Link"], as_index=False).agg({
+                'Text': lambda x: ' '.join(x)  
+            })
+    
+            return df_merged
+
+        # Wende die Funktion auf jede Gruppe von Spalte dup_column an
+        df_cleaned = df.groupby(dup_column, group_keys=False).apply(select_row)
+        df_cleaned.reset_index(inplace=True, drop=True)
+        df_cleaned = merge_text_on_adress(df_cleaned)
+        return df_cleaned
+            
 
 
 
